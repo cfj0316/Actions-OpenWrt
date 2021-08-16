80f058cdea7cac3e4964daeaa7c8cc1725e166d8
cfj0316 <82389513+cfj0316@users.noreply.github.com>
2021-08-16 16:04:06 +0800

    Update diy-part2.sh
    再次尝试下载工具编译

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,27 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.20/g' package/base-files/files/bin/config_generate
+
+rm -Rf tools/upx && svn export https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
+rm -Rf tools/ucl && svn export https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
+sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
+sed -i '$a /etc/sysupgrade.conf' package/base-files/files/lib/upgrade/keep.d/base-files-essential
+sed -i '$a /etc/acme' package/base-files/files/lib/upgrade/keep.d/base-files-essential
+sed -i '$a /etc/bench.log' package/base-files/files/lib/upgrade/keep.d/base-files-essential
+sed -i '/\/etc\/profile/d' package/base-files/files/lib/upgrade/keep.d/base-files-essential
+sed -i '/^\/etc\/profile/d' package/base-files/Makefile
+
+if [ -f sdk.tar.xz ]; then
+	sed -i 's,$(STAGING_DIR_HOST)/bin/upx,upx,' package/feeds/custom/*/Makefile
+	mkdir sdk
+	tar -xJf sdk.tar.xz -C sdk
+	cp -rf sdk/*/staging_dir/* ./staging_dir/
+	rm -rf sdk.tar.xz sdk
+	sed -i '/\(tools\|toolchain\)\/Makefile/d' Makefile
+	if [ -f /usr/bin/python ]; then
+		ln -sf /usr/bin/python staging_dir/host/bin/python
+	else
+		ln -sf /usr/bin/python3 staging_dir/host/bin/python
+	fi
+	ln -sf /usr/bin/python3 staging_dir/host/bin/python3
+fi
